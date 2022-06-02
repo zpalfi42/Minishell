2cf21982e17d090568c4e69041721bd7eb9d367a
Eloi Alonso quinonero <ealonso-@cbr15s2.42barcelona.com>
2022-06-02 18:38:02 +0200

    tokens saved btween spaces, left btween quotes yet

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ealonso- <ealonso-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 15:15:54 by zpalfi            #+#    #+#             */
-/*   Updated: 2022/06/02 17:13:04 by ealonso-         ###   ########.fr       */
+/*   Updated: 2022/06/02 18:33:16 by ealonso-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,71 @@ void	countq(t_data *data)
 		else if (data->line[i] == 39)
 			data->sc++;
 		i++;
+	}
+}
+
+void	save_tokens(t_data *data)
+{
+	int	i;
+	int	j;
+	int k;
+
+	k = 0;
+	j = 0;
+	i = 0;
+	data->tokens = (char **)malloc(sizeof(char *) * (data->count + 1));
+	if (!data->tokens)
+		exit (0);
+	data->tokens[data->count] = 0;
+	while (data->line[i] != '\0')
+	{
+		while (data->line[i] != ' ' && data->line[i] != '\0')
+		{
+			if (data->line[i] == 34 && data->dc > 1)
+			{
+				i++;
+				data->dc_2--;
+				while (data->line[i] != 34 && data->line[i] != '\0')
+					i++;
+					j++;
+				if (data->line[i] == 34)
+				{
+					data->dc_2--;
+					i++;
+					j++;
+				}
+			}
+			else if (data->line[i] == 39 && data->sc > 1)
+			{
+				i++;
+				data->sc_2--;
+				while (data->line[i] != 39 && data->line[i] != '\0')
+					i++;
+				if (data->line[i] == 39)
+				{
+					data->sc_2--;
+					i++;
+				}
+			}
+			else
+			{
+				i++;
+				j++;
+			}
+		}
+		data->tokens[data->word] = malloc(sizeof(char) * j);
+		data->tokens[data->word][j] = '\0';
+		while (j != 0)
+		{
+			data->tokens[data->word][k++] = data->line[i - j];
+			j--;
+		}
+		// printf("hi %s", data->tokens[1]);
+		data->word++;
+		k = 0;
+		j = 0;
+		if (data->line[i] == ' ')
+			i++;
 	}
 }
 
@@ -81,27 +146,30 @@ void	count_tokens(t_data *data)
 		}
 		if (data->line[i] == ' ')
 			i++;
-		printf("%c\n", data->line[i]);
+		// printf("%c\n", data->line[i]);
 		data->count++;
 	}
 }
 
 void	parser(t_data *data)
 {
-	// int	i;
+	int	i;
 
 	countq(data);
+	data->dc_2 = data->dc;
+	data->sc_2 = data->sc;
 	count_tokens(data);
-	printf("%d\n", data->count);
+	save_tokens(data);
+	// printf("%d\n", data->count);
 	// if (save_tokens(data))
 	// 	return (0);
-	// i = 0;
-	// while (data->tokens[i])
-	// {
-	// 	printf("%s\n", data->tokens[i]);
-	// 	i++;
-	// }
-	// free_all(data);
+	i = 0;
+	while (data->tokens[i])
+	{
+		printf("%s\n", data->tokens[i]);
+		i++;
+	}
+	free_all(data);
 }
 
 int main(int argc, char **argv, char **envp)
