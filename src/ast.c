f20f60984a632bfb39217200f24981ee511de41f
Zsolt Palfi <zpalfi@cbr15s2.42barcelona.com>
2022-06-08 15:39:41 +0200

    fclean

--- src/ast.c
+++ src/ast.c
@@ -6,7 +6,7 @@
 /*   By: zpalfi <zpalfi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 17:53:55 by zpalfi            #+#    #+#             */
-/*   Updated: 2022/06/08 15:35:58 by zpalfi           ###   ########.fr       */
+/*   Updated: 2022/06/08 15:39:11 by zpalfi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,30 +18,30 @@ void	check_cmd(t_data *data, t_ast *ast)
 	{
 		do_echo(data, ast);
 	}
-	else if (ft_strncmp(ast->cmd, "cd\0", 3) == 0)
-	{
-		do_cd(data, ast);
-	}
-	else if (ft_strncmp(ast->cmd, "pwd\0", 4) == 0)
-	{
-		do_pwd(data, ast);
-	}
-	else if (ft_strncmp(ast->cmd, "export\0", 7) == 0)
-	{
-		do_export(data, ast);
-	}
-	else if (ft_strncmp(ast->cmd, "unset\0", 6) == 0)
-	{
-		do_unset(data, ast);
-	}
-	else if (ft_strncmp(ast->cmd, "env\0", 4) == 0)
-	{
-		do_env(data, ast);
-	}
-	else if (ft_strncmp(ast->cmd, "exit\0", 5) == 0)
-	{
-		do_exit(data, ast);
-	}
+	// else if (ft_strncmp(ast->cmd, "cd\0", 3) == 0)
+	// {
+	// 	do_cd(data, ast);
+	// }
+	// else if (ft_strncmp(ast->cmd, "pwd\0", 4) == 0)
+	// {
+	// 	do_pwd(data, ast);
+	// }
+	// else if (ft_strncmp(ast->cmd, "export\0", 7) == 0)
+	// {
+	// 	do_export(data, ast);
+	// }
+	// else if (ft_strncmp(ast->cmd, "unset\0", 6) == 0)
+	// {
+	// 	do_unset(data, ast);
+	// }
+	// else if (ft_strncmp(ast->cmd, "env\0", 4) == 0)
+	// {
+	// 	do_env(data, ast);
+	// }
+	// else if (ft_strncmp(ast->cmd, "exit\0", 5) == 0)
+	// {
+	// 	do_exit(data, ast);
+	// }
 	else
 		ft_error(data, "Command not found :(");
 }
