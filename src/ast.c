0a0856bb1afde03ba9d4a232d660a277489523d7
Zsolt Palfi <zpalfi@cbr15s2.42barcelona.com>
2022-06-13 15:34:22 +0200

    deleted export functions in ast.c

--- src/ast.c
+++ src/ast.c
@@ -6,107 +6,11 @@
 /*   By: zpalfi <zpalfi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 17:53:55 by zpalfi            #+#    #+#             */
-/*   Updated: 2022/06/13 15:32:47 by zpalfi           ###   ########.fr       */
+/*   Updated: 2022/06/13 15:33:59 by zpalfi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-char	*export_name(t_data *data)
-{
-	char	*ret;
-	int		i;
-
-	i = 0;
-	while (data->tokens[1][i] != '=')
-		i++;
-	ret = malloc(sizeof(char) * i);
-	i = 0;
-	while (data->tokens[1][i] != '=')
-	{
-		ret[i] = data->tokens[1][i];
-		i++;
-	}
-	ret[i] = '\0';
-	return (ret);
-}
-
-char	*export_value(t_data *data)
-{
-	char	*ret;
-	int		i;
-
-	i = 0;
-	while (data->tokens[1][i] != '=')
-		i++;
-	i++;
-	int j = 0;
-	while (data->tokens[1][i + j] != '\0')
-		j++;
-	ret = malloc(sizeof(char) * j);
-	j = 0;
-	while (data->tokens[1][i + j] != '\0')
-	{
-		ret[j] = data->tokens[1][i + j];
-		j++;
-	}
-	ret[j] = '\0';
-	return (ret);
-}
-
-int	valid_export(t_data *data)
-{
-	int	i;
-	int	aux;
-
-	i = 1;
-	while(data->tokens[1][i])
-	{
-		if (data->tokens[1][i] == '=' && i != 0)
-			aux = 0;
-		i++;
-	}
-	return (aux);
-}
-
-void	change_value(t_data *data)
-{
-	char	*name;
-	t_list	*envo;
-
-	name = export_name(data);
-	envo = data->env;
-	while (data->env)
-	{
-		if (ft_strcmp(data->env->name, name))
-			break ;
-		data->env = data->env->next;
-	}
-	free(data->env->value);
-	data->env->value = export_value(data);
-	data->env = envo;
-	free(name);
-}
-
-int	export_exists(t_data *data)
-{
-	char	*name;
-	int		aux;
-	t_list	*envo;
-
-	aux = 0;
-	envo = data->env;
-	name = export_name(data);
-	while (data->env)
-	{
-		if (ft_strcmp(data->env->name, name))
-			aux = 1;
-		data->env = data->env->next;
-	}
-	data->env = envo;
-	free(name);
-	return (aux);
-}
 
 void	check_cmd(t_data *data, t_ast *ast)
 {
